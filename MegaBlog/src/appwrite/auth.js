059d94e29d9bda55b3205a3fd46204e95d5f8099
zarif007008 <zarif007008az@gmail.com>
2024-11-16 22:25:33 +0600

    authService

--- MegaBlog/src/appwrite/auth.js
+++ MegaBlog/src/appwrite/auth.js
@@ -4,6 +4,53 @@ import { Client, Account, ID } from "appwrite";
 export class AuthService {
     Client = new Client()
     Account
+
+    constructor() {
+        this.Client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId)
+        this.account = new Account(this.Client)
+
+    }
+
+    async createAccount ({email, password, name}) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name)
+            if (userAccount) {
+                // Try another method
+                return this.login({email, password})
+            } else {
+                return userAccount
+            }
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async login({email, password}){
+        try {
+            return await this.account.createEmailPasswordSession(email, password)
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return this.account.get()
+        } catch (error) {
+            throw error
+        }
+        return null
+    }
+
+    async logout() {
+        try {
+            return await this.account.deleteSessions()
+        } catch (error) {
+            throw error
+        }
+    }
 }
 
 const authService = new AuthService()
